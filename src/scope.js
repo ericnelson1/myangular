f0f0d79d7ca6fda03ec11a605b71d67ce6f7aaaf
ericnelson1 <ericnelson6@gmail.com>
2016-06-26 22:44:25 -0500

    updating scopes

--- src/scope.js
+++ src/scope.js
@@ -1,7 +1,35 @@
 'use strict';
 
-function Scope() {
+var _ = require('lodash');
 
+function Scope() {
+  this.$$watchers = [];
 };
+
+Scope.prototype.$watch = function(watchFn, listenerFn) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn
+  };
+  this.$$watchers.push(watcher);
+};
+
+Scope.prototype.$digest = function() {
+  var self = this;
+  var oldValue, newValue;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    oldValue = watcher.last;
+    if (newValue !== oldValue) {
+      watcher.last = newvalue;
+      watcher.listenerFn(newValue, oldValue, self);
+    }
+
+    watcher.listenerFn();
+  });
+};
+
+
+
 
 module.exports = Scope;
--- test/scope.spec.js
+++ test/scope.spec.js
@@ -9,6 +9,59 @@ describe('Scope', function(){
 
     expect(scope.aproperty).toBe(1);
   });
+
+  describe('digest', function() {
+    var scope;
+
+    beforeEach(function() {
+      scope = new Scope();
+    });
+
+    it('calls the listener function of a watch on first digest', function() {
+      // arrange
+      var watchFn = function() { return 'wat'; };
+      var listenerFn = jasmine.createSpy();
+      scope.$watch(watchFn, listenerFn);
+      // act
+      scope.$digest();
+      // assert
+      expect(listenerFn).toHaveBeenCalled();
+    });
+
+    it('calls a watch function with the scope as an argument', function() {
+      // arrange
+      var watchFn = jasmine.createSpy();
+      var listenerFn = function() {};
+      scope.$watch(watchFn, listenerFn);
+      // act
+      scope.$digest();
+      // assert
+      expect(watchFn).toHaveBeenCalled();
+    });
+
+    it('calls the listener function when the watched value changes', function() {
+      scope.somevalue = 'a';
+      scope.counter = 0;
+
+      scope.$watch(
+        function(scope) { return scope.somevalue; },
+        function(newValue, oldValue, scope) { scope.counter++; }
+      );
+
+      expect(scope.counter).toBe(0);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(1);
+      scope.somevalue = 'b';
+      expect(scope.counter).toBe(1);
+      scope.$digest();
+      expect(scope.counter).toBe(2);
+
+    });
+
+  });
+
 });
 
 
