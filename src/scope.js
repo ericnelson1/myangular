a362e2f061aa4e8e115b8bc525056c7c53050345
ericnelson1 <ericnelson6@gmail.com>
2016-06-27 01:00:23 -0500

    working on scopes, watch and digest

--- src/scope.js
+++ src/scope.js
@@ -4,31 +4,53 @@ var _ = require('lodash');
 
 function Scope() {
   this.$$watchers = [];
+  this.$$lastDirtyWatch = null;
 };
+
+function initWatchValue() {};
 
 Scope.prototype.$watch = function(watchFn, listenerFn) {
   var watcher = {
     watchFn: watchFn,
-    listenerFn: listenerFn
+    listenerFn: listenerFn || function() {},
+    last: initWatchValue
   };
   this.$$watchers.push(watcher);
+  this.$$lastDirtyWatch = null;
 };
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$digestOnce = function() {
   var self = this;
-  var oldValue, newValue;
+  var oldValue, newValue, dirty;
   _.forEach(this.$$watchers, function(watcher) {
     newValue = watcher.watchFn(self);
     oldValue = watcher.last;
     if (newValue !== oldValue) {
-      watcher.last = newvalue;
-      watcher.listenerFn(newValue, oldValue, self);
+      self.$$lastDirtyWatch = watcher;
+      watcher.last = newValue;
+      watcher.listenerFn(newValue, 
+        (oldValue === initWatchValue ? newValue : oldValue),
+        self);
+      dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;
     }
-
-    watcher.listenerFn();
   });
+  return dirty;
 };
 
+Scope.prototype.$digest = function() {
+  var ttl = 10;
+  var dirty;
+  this.$$lastDirtyWatch = null;
+
+  do {
+    dirty = this.$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw '10 digest iterations reached';
+    }
+  } while(dirty);
+}
 
 
 
